85e218f49fb7413518a4cdcf9281a54a4cf3ba6e
gavandrewj <gavinandrewgordonj@gmail.com>
2021-05-19 01:52:08 -0400

    updates

--- data-raw/changesinfo.R
+++ data-raw/changesinfo.R
@@ -1,6 +1,6 @@
 ## code to prepare `changesinfo` dataset goes here
 
-changesinfo <- data.frame(varname = names(dataset), changes = 0 )
+changesinfo <- data.frame(varname = names(dataset), changes = 'label applied' )
 
 changesinfo[changesinfo$varname == 'q0_0',]$changes <- 'Labels'
 changesinfo[changesinfo$varname == 'q0_1',]$changes <- 'Labels applied. All of the other option of is Black water or Black water Barima which are assumed the same'
@@ -8,5 +8,9 @@ changesinfo[changesinfo$varname == 'q0_2',]$changes <- 'Delete?'
 changesinfo[changesinfo$varname == 'q0_3',]$changes <- 'Recheck to determine the valid records'
 changesinfo[changesinfo$varname == 'q0_4',]$changes <- 'Labels'
 changesinfo[changesinfo$varname == 'q0_5',]$changes <- 'Labels applied. One of the other option is ddd. Unclear if this record was a test record. Poor signal category added'
+changesinfo[changesinfo$varname == 'q1_0',]$changes <- 'Changed to numeric. Large number of persons in one household (19). Seems like a legitimate record'
+changesinfo[changesinfo$varname == 'p1',]$changes <- 'Check name S and ttt'
+changesinfo[changesinfo$varname == 'p2',]$changes <- 'Check name A'
+changesinfo[changesinfo$varname == 'p3',]$changes <- 'Check name C, child1'
 
 usethis::use_data(changesinfo, overwrite = TRUE)
--- data-raw/dataset.R
+++ data-raw/dataset.R
@@ -3,6 +3,7 @@ library(readxl)
 library(tidyverse)
 
 rawsurveydata <- read_excel('C:/Users/gavin/Documents/GitHub/hesadsurvey/data-raw/HESAD_survey_-_all_versions_-_False_-_2021-05-18-13-12-26.xlsx')
+rawsurveydata <- filter(rawsurveydata,q0_5 == 1)
 usethis::use_data(rawsurveydata, overwrite = TRUE)
 
 #do not touch the above code
@@ -10,6 +11,9 @@ usethis::use_data(rawsurveydata, overwrite = TRUE)
 
 
 surveydataset <- read_excel('C:/Users/gavin/Documents/GitHub/hesadsurvey/data-raw/HESAD_survey_-_all_versions_-_False_-_2021-05-18-13-12-26.xlsx')
+
+#filter to remove records with no data
+
 
 #q0-0
 surveydataset$q0_0 <- factor(surveydataset$q0_0,
@@ -126,11 +130,201 @@ surveydataset$q0_5 <- factor(surveydataset$q0_5)
 attr(surveydataset$q0_5, "var.labels") <- 'Outcome of interview attempt'
 attr(surveydataset$q0_5,'label') <- 'Outcome of interview attempt'
 
+# filter out non response
+nonresponsesurveydataset <- filter(surveydataset,q0_5 != 'Respondent agreed to participate in the survey')
+surveydataset <- filter(surveydataset,q0_5 == 'Respondent agreed to participate in the survey')
+
 ##q0_5oth removed
 
 surveydataset <- surveydataset[,-10]
+surveydataset <- surveydataset[,-10]
 
 ##
+
+
+##q1_0
+surveydataset$q1_0 <- as.numeric(surveydataset$q1_0)
+attr(surveydataset$q1_0, "var.labels") <- 'How many members does your household have?'
+attr(surveydataset$q1_0,'label') <- 'How many members does your household have?'
+##
+
+##p1 - p20
+
+attr(surveydataset$p1, "var.labels") <- 'Name of Person One'
+attr(surveydataset$p1,'label') <- 'Name of Person One' 
+
+attr(surveydataset$p2, "var.labels") <- 'Name of Person Two'
+attr(surveydataset$p2,'label') <- 'Name of Person Two'
+
+attr(surveydataset$p3, "var.labels") <- 'Name of Person Three'
+attr(surveydataset$p3,'label') <- 'Name of Person Three'
+
+attr(surveydataset$p4, "var.labels") <- 'Name of Person Four'
+attr(surveydataset$p4,'label') <- 'Name of Person Four'
+
+attr(surveydataset$p5, "var.labels") <- 'Name of Person Five'
+attr(surveydataset$p5,'label') <- 'Name of Person Five'
+
+attr(surveydataset$p6, "var.labels") <- 'Name of Person Six'
+attr(surveydataset$p6,'label') <- 'Name of Person Six'
+
+attr(surveydataset$p7, "var.labels") <- 'Name of Person Seven'
+attr(surveydataset$p7,'label') <- 'Name of Person Seven'
+
+attr(surveydataset$p8, "var.labels") <- 'Name of Person Eight'
+attr(surveydataset$p8,'label') <- 'Name of Person Eight'
+
+attr(surveydataset$p9, "var.labels") <- 'Name of Person Nine'
+attr(surveydataset$p9,'label') <- 'Name of Person Nine'
+
+
+attr(surveydataset$p10, "var.labels") <- 'Name of Person Ten'
+attr(surveydataset$p10,'label') <- 'Name of Person Ten'
+
+attr(surveydataset$p11, "var.labels") <- 'Name of Person Eleven'
+attr(surveydataset$p11,'label') <- 'Name of Person Eleven'
+
+attr(surveydataset$p12, "var.labels") <- 'Name of Person Twelve'
+attr(surveydataset$p12,'label') <- 'Name of Person Twelve'
+
+attr(surveydataset$p13, "var.labels") <- 'Name of Person Thirteen'
+attr(surveydataset$p13,'label') <- 'Name of Person Thirteen'
+
+attr(surveydataset$p14, "var.labels") <- 'Name of Person Fourteen'
+attr(surveydataset$p14,'label') <- 'Name of Person Fourteen'
+
+attr(surveydataset$p15, "var.labels") <- 'Name of Person Fifteen'
+attr(surveydataset$p15,'label') <- 'Name of Person Fifteen'
+
+
+attr(surveydataset$p16, "var.labels") <- 'Name of Person Sixteen'
+attr(surveydataset$p16,'label') <- 'Name of Person Sixteen'
+
+attr(surveydataset$p17, "var.labels") <- 'Name of Person Seventeen'
+attr(surveydataset$p17,'label') <- 'Name of Person Seventeen'
+
+attr(surveydataset$p18, "var.labels") <- 'Name of Person Eighteen'
+attr(surveydataset$p18,'label') <- 'Name of Person Eighteen'
+
+attr(surveydataset$p19, "var.labels") <- 'Name of Person Nineteen'
+attr(surveydataset$p19,'label') <- 'Name of Person Nineteen'
+
+surveydataset <- surveydataset[,-30]
+
+
+## q10
+
+surveydataset$q10 <- factor(surveydataset$q10,
+                            levels = 1:4,
+                            labels = c(
+                              'Male headed',
+                              'Female headed',
+                              'Child headed (below age 18)/Orphan', 
+                              'Jointly headed by male and female'
+                              )
+                            )
+surveydataset$q10 <- factor(surveydataset$q10)
+
+attr(surveydataset$q10, "var.labels") <- 'Which of the following best describes your household?'
+attr(surveydataset$q10,'label') <- 'Which of the following best describes your household?'
+
+
+
+## q-10-1
+
+surveydataset$q10_1 <- factor(surveydataset$q10_1,
+                              levels = 1:4,
+                              labels = c(
+                                'Managed by a male',
+                                'Managed by a female',
+                                'Managed by a child (below age 18)/Orphan', 
+                                'Jointly managed by male and female'
+                                      )
+                              )
+
+surveydataset$q10_1 <- factor(surveydataset$q10_1)
+
+attr(surveydataset$q10_1, "var.labels") <- 'Which of the following best describes how your household is managed?'
+attr(surveydataset$q10_1,'label') <- 'Which of the following best describes how your household is managed?'
+
+
+## q11
+surveydataset$q11 <- factor(surveydataset$q11,
+                            levels = c(0,1,98,99),
+                            labels = c(
+                              'No',
+                              'Yes',
+                              "Don't know",
+                              'No answer'
+
+                            )
+                            )
+
+surveydataset$q11 <- factor(surveydataset$q11)
+
+attr(surveydataset$q11, "var.labels") <- 'Have one or more household members participated in the development or updating of a community development plan, village investment plan or similar types of plans **in the last 12 months?**'
+attr(surveydataset$q11,'label') <- 'Have one or more household members participated in the development or updating of a community development plan, village investment plan or similar types of plans **in the last 12 months?**'
+
+## q12
+ surveydataset[surveydataset$q11 != 'Yes','q12'] <- '-3'
+
+surveydataset$q12 <- factor(surveydataset$q12,
+                            levels = c(0,1,98,99,-3),
+                            labels = c(
+                              'No',
+                              'Yes',
+                              "Don't know",
+                              'No answer',
+                              'Not Applicable'
+                            )
+)
+
+surveydataset$q12 <- factor(surveydataset$q12)
+
+attr(surveydataset$q12, "var.labels") <- 'Were actions to reduce the impacts from climate events (such as floods, salt water intrusion or periods with unusual high temperatures) considered in this plan, in which you participated?'
+attr(surveydataset$q12,'label') <- 'Were actions to reduce the impacts from climate events (such as floods, salt water intrusion or periods with unusual high temperatures) considered in this plan, in which you participated?'
+
+
+
+## q13
+surveydataset$q13 <- factor(surveydataset$q13,
+                            levels = c(0,1,98,99),
+                            labels = c(
+                              'No',
+                              'Yes',
+                              "Don't know",
+                              'No answer'
+                            )
+)
+
+surveydataset$q13 <- factor(surveydataset$q13)
+
+attr(surveydataset$q13, "var.labels") <- 'Is at least one household member participating in a community or producer group that has an adopted business plan?'
+attr(surveydataset$q13,'label') <- 'Is at least one household member participating in a community or producer group that has an adopted business plan?'
+
+
+#q14
+surveydataset[surveydataset$q13 != 'Yes','q14'] <- '-3'
+
+surveydataset$q14 <- factor(surveydataset$q14,
+                            levels = c(0,1,98,99,-3),
+                            labels = c(
+                              'No',
+                              'Yes',
+                              "Don't know",
+                              'No answer',
+                              'Not Applicable'
+                            )
+)
+
+surveydataset$q14 <- factor(surveydataset$q14)
+
+attr(surveydataset$q14, "var.labels") <- 'Has the business plan contributed to an increase in the household incomes in the last twelve months?'
+attr(surveydataset$q14,'label') <- 'Has the business plan contributed to an increase in the household incomes in the last twelve months?'
+
+
+
+
 
 varlabel <- c(
   'start',
@@ -143,7 +337,33 @@ varlabel <- c(
   'Household ID',
   'Interview Mode',
   'Outcome of interview attempt',
-  rep('eh',347)
+  'How many members does your household have?',
+  'Name of Person One',
+  'Name of Person Two',
+  'Name of Person Three',
+  'Name of Person Four',
+  'Name of Person Five',
+  'Name of Person Six',
+  'Name of Person Seven',
+  'Name of Person Eight',
+  'Name of Person Nine',
+  'Name of Person Ten',
+  'Name of Person Eleven',
+  'Name of Person Twelve',
+  'Name of Person Thirteen',
+  'Name of Person Fourteen',
+  'Name of Person Fifteen',
+  'Name of Person Sixteen',
+  'Name of Person Seventeen',
+  'Name of Person Eighteen',
+  'Name of Person Nineteen',
+  'Which of the following best describes your household?',
+  'Which of the following best describes how your household is managed?',
+  'Have one or more household members participated in the development or updating of a community development plan, village investment plan or similar types of plans **in the last 12 months?**',
+  'Were actions to reduce the impacts from climate events (such as floods, salt water intrusion or periods with unusual high temperatures) considered in this plan, in which you participated?',
+  'Is at least one household member participating in a community or producer group that has an adopted business plan?', 
+  'Has the business plan contributed to an increase in the household incomes in the last twelve months?',
+  rep('eh',318)
 )
 
 attr(surveydataset, "var.labels") <- varlabel
